881d32953ad25340784e362429b531e131f226dc
shivamgohri <shivamgohri93@gmail.com>
2020-04-17 23:53:28 +0530

    adding

---
+++ Arrays/beggar_query.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int n;
+	cin>> n; 
+
+	int* arr = new int[n]();
+
+	int k;
+	cin>> k;
+
+	while(k--){
+
+		int start, end, money;
+		cin>> start >>end >>money;
+
+		if(start!=0){
+			arr[start]
+		}
+
+	}
+}
