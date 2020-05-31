88ea36864a9bc8c01ee90882134b387ff0789e9f
shivamgohri <shivamgohri93@gmail.com>
2020-06-01 01:11:56 +0530

    add

--- Graph/template.cpp
+++ Graph/template.cpp
@@ -18,7 +18,8 @@ void testcases(){
 
 int main(){
 
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);    cout.tie(NULL);
 
 	int t = 1;
 	// cin>> t;
