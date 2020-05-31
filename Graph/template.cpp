6b02099c750767a36426368daaa29410605e2194
shivamgohri <shivamgohri93@gmail.com>
2020-05-31 23:00:31 +0530

    add

---
+++ Graph/template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long int
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define MAX(int)1e7+5
+
+
+void testcases(){
+
+
+
+}
+
+
+int main(){
+
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int t = 1;
+	// cin>> t;
+
+	while(t--){
+		testcases();
+		cout<<endl;
+	}
+
+	return 0;
+}
